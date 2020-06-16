19d934c6ea50f51b13fde79ffae9a3f7c32bb0ba
JuanJoseMora <juanmora.dev@gmail.com>
2020-06-16 09:54:01 +0200

    version 1.02

--- index.js
+++ index.js
@@ -16,108 +16,91 @@ btnFooter.onclick = function(){
 span.onclick = function(){
     modal.style.display = "none";
 }
-
-/* MESSAGE POP UP MODAL BOX */
-aboutMeBtn = document.getElementById('contact-link-about');
-aboutMeBtn.onclick = function(){
-    let el = document.getElementById('modal-box-gallery-id');
-    el.classList.toggle('hidden-el');
-}
-
-portfolioBtn = document.getElementById('contact-link-portfolio');
-portfolioBtn.onclick = function(){
-    let el = document.getElementById('modal-box-gallery-id');
-    el.classList.toggle('hidden-el');
-}
-
-closeMainModal = document.getElementById('main-modal-box-close');
-closeMainModal.onclick = function(){
-    let el = document.getElementById('modal-box-gallery-id');
-    el.classList.toggle('hidden-el');
-}
-
-/* LANGUAGE CONFIG */
-
-let language;
-// window.onload = function (){
-//     language = document.getElementById("language").textContent;
-//     profTitle = document.getElementById("prof-title").textContent;
-//     console.log("language: " + language);
-//     console.log("prof title: " + profTitle);
-//     if(language == "EN"){
-//         profTitle = "Web developer";
-//     } else if (language == "ES"){
-//         profTitle = "Desarrollo Web";
-//         console.log("español");
-//     } else if (language === "FR"){
-//         profTitle = "Développeur -Web";
-//         console.log("español");
-//     }
-// }
-
-window.onload = function(){
-    language = document.getElementById("language").textContent;
-    
-    if(language == "EN"){
-        document.getElementById("prof-title").textContent = "Web developer";
-        document.getElementById("main-title-hide").textContent = "Hello I'm Juanjo";
-        document.getElementById("sub-title").textContent = "Welcome to my website";
-        document.getElementById("contact-link-about").textContent = "About me";
-        document.getElementById("contact-link-portfolio").textContent = "My Portfolio";
-        document.getElementById("contact-link").textContent = "Contact";
-        document.getElementById("skills-title").textContent = "SKILLS";
-        // document.getElementById("home-link").textContent = "Home";
-        document.getElementById("gallery-link").textContent = "Gallery";
-        document.getElementById("contact-link-footer").textContent = "Contact";
+       
         
-        
-    } else if (language == "ES"){
-        document.getElementById("prof-title").textContent = "Desarrollo Web";
-        document.getElementById("main-title-hide").textContent = "Hola soy Juanjo";
-        document.getElementById("sub-title").textContent = "Bienvenido a mi web";
-        document.getElementById("contact-link-about").textContent = "Sobre mi";
-        document.getElementById("contact-link-portfolio").textContent = "Mi Portafolio";
-        document.getElementById("contact-link").textContent = "Contacto";
-        document.getElementById("skills-title").textContent = "COMPETENCIAS";
-        // document.getElementById("home-link").textContent = "Inicio";
-        document.getElementById("gallery-link").textContent = "Galería";
-        document.getElementById("contact-link-footer").textContent = "Contacto";
 
-    } else if (language === "FR"){
-        document.getElementById("prof-title").textContent = "Développeur Web";
-        document.getElementById("main-title-hide").textContent = "Salut c'est Juanjo";
-        document.getElementById("sub-title").textContent = "Bienvenu(e) à mon site";
-        document.getElementById("contact-link-about").textContent = "Moi";
-        document.getElementById("contact-link-portfolio").textContent = "Mon Portfolio";
-        document.getElementById("contact-link").textContent = "Contact";
-        document.getElementById("skills-title").textContent = "COMPETENCES";
-        // document.getElementById("home-link").textContent = "Accueil";
-        document.getElementById("gallery-link").textContent = "Galerie";
-        document.getElementById("contact-link-footer").textContent = "Contact";
+//type writer effect
+var text;
+
+hideText = document.getElementById("main-title-hide");
+text = document.getElementById("main-title");
+let str = hideText.textContent;
+
+let speed = 200;
+let i = 0;
+
+function typeWriter(){
+    if(i< str.length){
+        text.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typeWriter,speed);
+    } else {
+        document.getElementById('sub-title').style.color = "black";
     }
+}
+setTimeout(typeWriter(),speed)
 
-    //type writer effect
-    var text;
 
-    hideText = document.getElementById("main-title-hide");
-    text = document.getElementById("main-title");
-    let str = hideText.textContent;
 
-    let speed = 200;
-    let i = 0;
 
-    function typeWriter(){
-        if(i< str.length){
-            text.innerHTML += str.charAt(i);
-            i++;
-            setTimeout(typeWriter,speed);
-        } else {
-            document.getElementById('sub-title').style.color = "black";
+
+//ALPINE.JS
+
+function main(){
+    return{
+        portfopen: false,
+        aboutopen: false,
+        giuropen: false,
+        tastyopen: false,
+        notasopen: false,
+        sudoku4x4open: false,
+        puzzlespdfopen: false,
+        artgalopen: false,
+    
+        about: "About me",
+        portfolio: "Portfolio",
+        mainTitle: "Hello I'm Juanjo",
+        mainSubTitle: "Welcome to my website",
+        webDev: "Web developer",
+        skills: "SKILLS",
+        gallery: "Gallery",
+        contact: "Contact",
+        visit: "Visit",
+        spanish: function(){
+            this.about = "Sobre mí";
+            this.portfolio = "Portafolio";
+            this.mainTitle = "Hola soy Juanjo";
+            this.mainSubTitle = "Bienvenido a mi web";
+            this.webDev = "Desarrollo Web";
+            this.skills = "COMPETENCIAS";
+            this.gallery = "Galería";
+            this.contact = "Contacto";
+            this.visit = "Visitar";
+        },
+        french: function(){
+            this.about = "Moi";
+            this.portfolio = "Portfolio";
+            this.mainTitle = "Salut, c'est Juanjo";
+            this.mainSubTitle = "Bienvenue à mon site";
+            this.webDev = "Développeur Web";
+            this.skills = "COMPETENCES";
+            this.gallery = "Gallerie";
+            this.contact = "Contact";
+            this.visit = "Visiter";
+        },
+        english: function(){
+            this.about = "About me";
+            this.portfolio = "Portfolio";
+            this.mainTitle = "Hello I'm Juanjo";
+            this.mainSubTitle = "Welcome to my website";
+            this.webDev = "Web developer";
+            this.skills = "SKILLS";
+            this.gallery = "Gallery";
+            this.contact = "Contact";
+            this.visit = "Visit";
+
         }
     }
-    setTimeout(typeWriter(),speed)
 }
-
-
 
 
